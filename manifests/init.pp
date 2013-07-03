ed4e793627cc87c675c8875f298cba0f54059c69
Mathieu Parent <math.parent@gmail.com>
2013-07-03 16:33:26 +0200

    Concat as no $ensure, workaround

--- manifests/init.pp
+++ manifests/init.pp
@@ -360,27 +360,41 @@ class bind (
 
     include concat::setup
 
-    concat { $bind::config_file:
-      ensure  => $bind::manage_file,
-      mode    => $bind::config_file_mode,
-      owner   => $bind::config_file_owner,
-      group   => $bind::config_file_group,
-      require => Package[$bind::package],
-      notify  => $bind::manage_service_autorestart,
-      audit   => $bind::manage_audit,
-      noop    => $bind::bool_noops,
-    }
-    concat::fragment { 'bind_head':
-      ensure  => $bind::manage_file,
-      target  => $bind::config_file,
-      order   => '01',
-      mode    => $bind::config_file_mode,
-      owner   => $bind::config_file_owner,
-      group   => $bind::config_file_group,
-      source  => $bind::manage_file_source,
-      content => $bind::manage_file_content,
-      audit   => $bind::manage_audit,
-      noop    => $bind::bool_noops,
+    if $bind::manage_file_source or $bind::manage_file_content {
+      concat { $bind::config_file:
+        mode    => $bind::config_file_mode,
+        owner   => $bind::config_file_owner,
+        group   => $bind::config_file_group,
+        require => Package[$bind::package],
+        notify  => $bind::manage_service_autorestart,
+        audit   => $bind::manage_audit,
+        noop    => $bind::bool_noops,
+      }
+      concat::fragment { 'bind_head':
+        target  => $bind::config_file,
+        order   => '01',
+        mode    => $bind::config_file_mode,
+        owner   => $bind::config_file_owner,
+        group   => $bind::config_file_group,
+        source  => $bind::manage_file_source,
+        content => $bind::manage_file_content,
+        audit   => $bind::manage_audit,
+        noop    => $bind::bool_noops,
+      }
+    } else {
+      file { $bind::config_file:
+        ensure  => $bind::manage_file,
+        path    => $bind::config_file,
+        mode    => $bind::config_file_mode,
+        owner   => $bind::config_file_owner,
+        group   => $bind::config_file_group,
+        require => Package[$bind::package],
+        notify  => $bind::manage_service_autorestart,
+        replace => $bind::manage_file_replace,
+        audit   => $bind::manage_audit,
+        noop    => $bind::bool_noops,
+      }
+
     }
 
   }
